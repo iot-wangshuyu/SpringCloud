3c4591ada21ffe25667b699afc4b52c30242577c
shuyu.wang <iot_wangshuyu@126.com>
2018-02-10 13:59:49 +0800

    springboot 读写分离

---
+++ demo/src/main/java/com/ganinfo/config/CorsConfig.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.ganinfo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @ClassName: CorsConfig 
+ * @Description: TODO
+ * @author shuyu.wang
+ * @date 2018年1月23日 下午6:21:31 
+ * @version V1.0
+ */
+@Configuration  
+public class CorsConfig extends WebMvcConfigurerAdapter{
+	@Override    
+    public void addCorsMappings(CorsRegistry registry) {    
+        registry.addMapping("/**")    
+                .allowedOrigins("*")    
+                .allowCredentials(true)    
+                .allowedMethods("GET", "POST", "DELETE", "PUT")    
+                .maxAge(3600);    
+    }    
+}
